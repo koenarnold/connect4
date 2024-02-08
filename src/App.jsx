9b99dbe10625332d09fea1aaa7102488eddd65fa
Koen Arnold <kostarnold@gmail.com>
2024-02-07 19:56:46 -0500

    started initialization of player/game setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from 'react'
 import './styles/App.css'
+import Setup from '/src/components/Setup'
 
 function App() {
 
-  return (
-    <>
-    </>
-  )
+  const [page, setPage] = useState(0)
+  const [playerOneName, setPlayerOneName] = useState('Player 1')
+  const [playerTwoName, setPlayerTwoName] = useState('Player 2')
+
+switch (page) {
+  case 0:
+    return (
+      <>
+        <Setup playerOneName={playerOneName} setPlayerOneName={setPlayerOneName} playerTwoName={playerTwoName} setPlayerTwoName={setPlayerTwoName}/>
+      </>
+    )
+  case 1:
+    break;
+}
+
 }
 
 export default App
--- src/components/Setup.jsx
+++ src/components/Setup.jsx
@@ -1,8 +1,19 @@
 import {useState} from 'react'
+import '/src/styles/Setup.css'
 
-export default function Setup () {
+export default function Setup ({playerOneName, setPlayerOneName, playerTwoName, setPlayerTwoName}) {
 
   return (
-    <></>
+    <div>
+      <h1>Game Setup</h1>
+      <div className="player-setup-container">
+        <div className="player-setup">
+          <h2>{playerOneName}</h2>
+        </div>
+        <div className="player-setup">
+          <h2>{playerTwoName}</h2>
+        </div>
+      </div>
+    </div>
   )
 }
