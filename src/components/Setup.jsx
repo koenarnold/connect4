3ef1212a8b1db04ed55cda77c7aca9b200e60b58
Koen Arnold <kostarnold@gmail.com>
2024-02-18 07:55:21 -0500

    working on setup

--- src/components/Setup.jsx
+++ src/components/Setup.jsx
@@ -10,9 +10,27 @@ export default function Setup ({playerOneName, setPlayerOneName, playerTwoName, 
       <div className="player-setup-container">
         <div className="player-setup">
           <h2>{playerOneName}</h2>
+          <input type="text" onChange={(e)=>{setPlayerOneName(e.target.value)}} style={{marginBottom: "5vh"}}/>
+          <div className="color-selector-container">
+            <div className="circle" style={{backgroundColor: "#f23224"}}></div>
+            <div className="circle" style={{backgroundColor: "#c619e0"}}></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+          </div>
         </div>
         <div className="player-setup">
           <h2>{playerTwoName}</h2>
+          <input type="text" onChange={(e)=>{setPlayerTwoName(e.target.value)}} style={{marginBottom: "5vh"}}/>
+          <div className="color-selector-container">
+          <div className="circle" style={{backgroundColor: "#f23224"}}></div>
+            <div className="circle" style={{backgroundColor: "#c619e0"}}></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+          </div>
         </div>
       </div>
     </div>
